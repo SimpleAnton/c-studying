5e1b5e9fce3d0857ffdb1405ff7c5c7d40ca9ee0
SimpleAnton <dautpaevermek@gmail.com>
2025-08-26 23:23:09 +0300

    Task1 done

--- Homeworks/Hw2/Task1.c
+++ Homeworks/Hw2/Task1.c
@@ -1,11 +1,41 @@
+#include <math.h>
 #include<stdio.h>
 
 int main() {
 
-    int imp;
+    int a=1;
+    int b=1;
 
-    for (int i=0; i<=; i++) {
-        printf("Hello World");
+    do {
+        if (a<=0) printf("That's not appropriate number. Choose something different.\n");
+        printf("Please enter a number: \n");
+        scanf("%d",&a);
+    } while (a<=0);
+
+    do {
+        if (b<=0) printf("That's not appropriate number. Choose something different.\n");
+        printf("Please enter another number: \n");
+        scanf("%d",&b);
+    } while (b<=0);
+
+    int min = a>b?b:a;
+    int max = a>b?a:b;
+
+    printf("The minimum number entered is %d\nThe maximum number entered is %d\n",min,max);
+
+    int temp1=1;
+    int temp2=0;
+    int temp3=0;
+
+    for (int i=1; i<=max; i++) {
+
+        temp3 = temp1+temp2;
+
+        if (min<=i) {
+            printf("%d ",temp3);
+        }
+        temp1=temp2;
+        temp2=temp3;
     }
 
 }
